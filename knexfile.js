251770d8745628ab280e0e005b31cbe8527a6ed1
probiss <bulut.cihad@gmail.com>
2023-03-03 10:40:38 +0300

    go

---
+++ knexfile.js
@@ -0,0 +1,58 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./dev.sqlite3",
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        // sqlite engine'e bağlandığımızda aşağıdaki kod çalışacak:
+        conn.run("PRAGMA foreign_keys = ON", done); // FK kullanımını açmaya zorlayacak
+      },
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+};
